5c645663cd16c742a49c983cfd1187653802b048
Patrick Carney <patrick.s.carney@gmail.com>
2021-04-06 16:00:53 -0400

    updates

--- src/hello_openshift.go
+++ src/hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "aHello OpenShift!"
+		response = "Hello OpenShift!"
 	}
 
 	fmt.Fprintln(w, response)
