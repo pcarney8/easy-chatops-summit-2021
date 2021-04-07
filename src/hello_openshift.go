bca1526b052208f1a9a09da18e2d9523e6d40580
Patrick C <pcarney8@users.noreply.github.com>
2021-04-07 12:43:13 -0400

    Update hello_openshift.go

--- src/hello_openshift.go
+++ src/hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift new!"
+		response = "Hello OpenShift!"
 	}
 
 	fmt.Fprintln(w, response)
